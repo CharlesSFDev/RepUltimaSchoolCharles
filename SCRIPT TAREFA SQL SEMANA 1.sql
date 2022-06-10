bcd30923d80640dce95cb254a3d79b559439a606
Charles Fortunato <97842186+CharlesSFDev@users.noreply.github.com>
2022-06-09 21:39:13 -0300

    Add files via upload

---
+++ SCRIPT TAREFA SQL SEMANA 1.sql
@@ -0,0 +1,16 @@
+--CREATE TABLE fornecedor
+--	(id INT,
+--	nome VARCHAR(50),
+--	endereco VARCHAR(150));
+
+--INSERT INTO fornecedor
+--	VALUES(3,'Padaria do Pão', 'Rua dos Leites, 47'),
+--	(4,'Marcenaria Martelo', 'Rua dos Leites, 150');
+
+--UPDATE fornecedor 
+--	SET endereco = 'Rua dos Peixes, 26'
+--	WHERE id=2;	
+
+-- SELECT * FROM fornecedor;
+
+-- DELETE FROM fornecedor WHERE id = 1;
